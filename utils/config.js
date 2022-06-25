660635bf896e0f329bc63e4ed3db65e1d3a100ea
xukooru <you@example.comclear>
2022-06-26 04:09:14 +0500

    Connect to Sanity.io

---
+++ utils/config.js
@@ -0,0 +1,8 @@
+const config = {
+  projectId: 'e67folxk',
+  dataset: 'production',
+  apiVersion: '2021-03-25', // use current UTC date - see "specifying API version"!
+  //   token: 'sanity-auth-token', // or leave blank for unauthenticated usage
+};
+
+export default config;
